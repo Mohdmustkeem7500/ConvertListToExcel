957d9760bdd6d5e84dc1990b4f318c99c0d0c8c2
Mohd Mustkeem <mohdmustkeem78624@gmail.com>
2022-06-28 18:03:49 +0530

    Add Data

--- MainApp/Model/UserManager.cs
+++ MainApp/Model/UserManager.cs
@@ -45,12 +45,84 @@ namespace MainApp.Model
                     Username = "TestUsername3",
                     Password = "123",
                     CreatedOn = DateTime.UtcNow
-                },new UserManager
+                },
+                new UserManager
                 {
                     Id = Guid.NewGuid().ToString(),
                     FirstName = "Test 4",
                     LastName = "Test 4",
                     Username = "TestUsername4",
+                    Password = "123",
+                    CreatedOn = DateTime.UtcNow
+                },
+                new UserManager
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    FirstName = "Test 5",
+                    LastName = "Test 5",
+                    Username = "TestUsername5",
+                    Password = "123",
+                    CreatedOn = DateTime.UtcNow
+                },
+                new UserManager
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    FirstName = "Test 6",
+                    LastName = "Test 6",
+                    Username = "TestUsername6",
+                    Password = "123",
+                    CreatedOn = DateTime.UtcNow
+                },
+                new UserManager
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    FirstName = "Test 7",
+                    LastName = "Test 7",
+                    Username = "TestUsername7",
+                    Password = "123",
+                    CreatedOn = DateTime.UtcNow
+                },
+                new UserManager
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    FirstName = "Test 8",
+                    LastName = "Test 8",
+                    Username = "TestUsername8",
+                    Password = "123",
+                    CreatedOn = DateTime.UtcNow
+                },
+                new UserManager
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    FirstName = "Test 9",
+                    LastName = "Test9 ",
+                    Username = "TestUsername9",
+                    Password = "123",
+                    CreatedOn = DateTime.UtcNow
+                },
+                new UserManager
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    FirstName = "Test 10",
+                    LastName = "Test 10",
+                    Username = "TestUsername10",
+                    Password = "123",
+                    CreatedOn = DateTime.UtcNow
+                },
+                new UserManager
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    FirstName = "Test 11",
+                    LastName = "Test 11",
+                    Username = "TestUsername11",
+                    Password = "123",
+                    CreatedOn = DateTime.UtcNow
+                },new UserManager
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    FirstName = "Test 12",
+                    LastName = "Test 12",
+                    Username = "TestUsername12",
                     Password = "123",
                     CreatedOn = DateTime.UtcNow
                 }
