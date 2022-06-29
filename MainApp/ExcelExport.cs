beac0f5051b278508d75fc27b479d0e244b1dd0a
Mohd Mustkeem <mohdmustkeem78624@gmail.com>
2022-06-29 14:17:52 +0530

    Add code Auto formate

--- MainApp/ExcelExport.cs
+++ MainApp/ExcelExport.cs
@@ -15,16 +15,56 @@ namespace MainApp
             Excel.Workbook excelWorkBook = excelApp.Workbooks.Add();
             Excel._Worksheet xlWorksheet = excelWorkBook.Sheets[1];
             Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            
+
+            //foreach (Excel.Range column in selectedRange.Columns)
+            //{
+            //    column.ColumnWidth = (double)column.ColumnWidth + 5;
+            //}
+
+
+
             foreach (DataTable table in dataSet.Tables)
             {
                 //Add a new worksheet to workbook with the Datatable name
                 Excel.Worksheet excelWorkSheet = excelWorkBook.Sheets.Add();
                 excelWorkSheet.Name = table.TableName;
 
+
+                //Excel.Range selectedRange = (Excel.Range)excelApp.Selection;
+                //selectedRange.Columns.AutoFit();
+                //foreach (Excel.Range column in selectedRange.Columns)
+                //{
+                //    column.ColumnWidth = (double)column.ColumnWidth + 40;
+                //}
+
+
+                for (int j = 1; j <= table.Columns.Count; j++)
+                {
+                    Excel.Range selectedRange = (Excel.Range)excelApp.Selection;
+                    Excel.Range column = selectedRange.Columns[j];
+                    selectedRange.Columns.AutoFit();
+
+                    //column.ColumnWidth = (double)column.ColumnWidth + 40;
+
+                    int p = 1;
+                    if (p == j)
+                    {
+                        column.ColumnWidth = (double)column.ColumnWidth + 30;
+                    }
+                    else
+                    {
+                        column.ColumnWidth = (double)column.ColumnWidth + 10;
+                    }
+
+                }
+
                 // add all the columns
                 for (int i = 1; i < table.Columns.Count + 1; i++)
                 {
                     excelWorkSheet.Cells[1, i] = table.Columns[i - 1].ColumnName;
+                    
                 }
 
                 // add all the rows
